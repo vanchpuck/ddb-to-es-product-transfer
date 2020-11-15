91a329e3770f685a88a84516454cbedf0d84ef9b
Ivan Zolotov <vanchpuck@mail.ru>
2020-11-15 22:45:33 +0300

    Passing tests

--- lambda_function.py
+++ lambda_function.py
@@ -9,23 +9,25 @@ logging.getLogger().setLevel(logging.INFO)
 
 MODEL_DATA_BUCKET = "org.gear-scanner.data"
 CLASSIFIER = Classifier(MODEL_DATA_BUCKET)
+PRODUCT_INDEX = "products"
+ORIGINAL_INDEX = "originals"
 
 
 def lambda_handler(event, context):
     logging.info(event)
     es_host = os.environ['ES_HOST']
-    es_index = os.environ['ES_INDEX']
-    es_type = os.environ['ES_TYPE']
+    es_product_index = os.environ.get('ES_PRODUCT_INDEX', '')
+    es_original_index = os.environ.get('ES_ORIGINAL_INDEX', '')
 
     es = Elasticsearch(es_host, verify_certs=False)
 
-    logging.info("Storing product data to {} index.".format(es_index))
-    helpers.bulk(es, records_generator(event['Records'], es_index, es_type))
+    # logging.info("Storing product data to {} index.".format(es_index))
+    helpers.bulk(es, records_generator(event['Records']))
 
     return 'SUCCESS'
 
 
-def records_generator(records, index, type):
+def records_generator(records):
     for record in records:
         doc_id = record['dynamodb']['Keys']['url']['S']
         if record["eventName"] in ["INSERT", "MODIFY"]:
@@ -34,25 +36,45 @@ def records_generator(records, index, type):
                 logging.info('{} record does not contain the document field. Skipping.'.format(doc_id))
                 continue
             document = new_image['document']['M']
-            if 'name' not in document or 'NULL' in document['name']:
-                logging.info('{} record has no product information. Skipping.'.format(doc_id))
-                continue
-            name = document['name']['S']
-            brand = document['brand']['S'].lower()
-            url = document['url']['S']
-            es_doc = {
-                "_index": index,
-                "_type": type,
-                "_id": url,
-                'url': document['url']['S'],
-                'store': document['store']['S'],
-                'name': name,
-                'normalizedName': CLASSIFIER.classify(brand, name),
-                'price': document['price']['N'],
-                'currency': document['currency']['S'],
-                'imageUrl': document['imageUrl']['S'],
-            }
-            if 'oldPrice' in document:
-                es_doc['oldPrice'] = document['oldPrice']['N']
-            logging.info('{} record marked to be sent to Elasticsearch.'.format(str(es_doc)))
-            yield es_doc
+            if 'data' in document and 'kind' in document:
+                data = document['data']['M']
+                kind = document['kind']['S']
+                if kind == 'product':
+                    logging.info('The product record spotted')
+                    name = data['name']['S']
+                    brand = data['brand']['S'].lower()
+                    url = data['url']['S']
+                    es_doc = {
+                        "_index": PRODUCT_INDEX,
+                        "_type": "product",
+                        "_id": url,
+                        'url': data['url']['S'],
+                        'store': data['store']['S'],
+                        'name': name,
+                        'brand': brand,
+                        'original': CLASSIFIER.classify(brand, name),
+                        'price': data['price']['N'],
+                        'currency': data['currency']['S'],
+                        'imageUrl': data['imageUrl']['S']
+                    }
+                    if 'oldPrice' in data:
+                        es_doc['oldPrice'] = data['oldPrice']['N']
+                    logging.info('{} record marked to be sent to Elasticsearch.'.format(str(es_doc)))
+                    yield es_doc
+                elif kind == 'originalCategory':
+                    logging.info('The original record spotted')
+                    original_product_list = data['products']['L']
+                    for original_product in original_product_list:
+                        name = original_product['M']['name']['S']
+                        brand = original_product['M']['brand']['S'].lower()
+                        es_doc = {
+                            "_index": ORIGINAL_INDEX,
+                            "_type": "original",
+                            "_id": name,
+                            'name': name,
+                            'brand': brand,
+                        }
+                        logging.info('{} record marked to be sent to Elasticsearch.'.format(str(es_doc)))
+                        yield es_doc
+                else:
+                    logging.warning('{} record has unknown product type. Skipping.'.format(doc_id))
--- test/test_integration.py
+++ test/test_integration.py
@@ -5,12 +5,15 @@ from elasticsearch import Elasticsearch
 from time import sleep
 
 ELASTIC_HOST = "http://localhost:9200"
-INDEX = "gear"
-DOC_TYPE = "product"
+PRODUCT_INDEX = "products"
+ORIGINAL_INDEX = "originals"
+# INDEX = "or"
+PRODUCT_DOC_TYPE = "product"
+ORIGINAL_DOC_TYPE = "original"
 
 os.environ["ES_HOST"] = ELASTIC_HOST
-os.environ["ES_INDEX"] = INDEX
-os.environ["ES_TYPE"] = DOC_TYPE
+# os.environ["ES_PRODUCT_INDEX"] = INDEX
+# os.environ["ES_TYPE"] = DOC_TYPE
 
 event = {
     "Records": [
@@ -25,32 +28,37 @@ event = {
                 "NewImage": {
                     "document": {
                         "M": {
-                            "url": {
-                                "S": "http://test.com/product_1"
-                            },
-                            "store": {
-                                "S": "test_store"
-                            },
-                            "brand": {
-                                "S": "petzl"
-                            },
-                            "name": {
-                                "S": "lynx"
-                            },
-                            "price": {
-                                "N": 10.99
-                            },
-                            "oldPrice": {
-                                "N": 12.99
-                            },
-                            "currency": {
-                                "S": "USD"
-                            },
-                            "imageUrl": {
-                                "S": "http://test.com/image_1.jpg"
-                            },
-                            "parseError": {
-                                "NULL": True
+                            "kind": {"S": "product"},
+                            "data": {
+                                "M": {
+                                    "url": {
+                                        "S": "http://test.com/product_1"
+                                    },
+                                    "store": {
+                                        "S": "test_store"
+                                    },
+                                    "brand": {
+                                        "S": "petzl"
+                                    },
+                                    "name": {
+                                        "S": "lynx"
+                                    },
+                                    "price": {
+                                        "N": 10.99
+                                    },
+                                    "oldPrice": {
+                                        "N": 12.99
+                                    },
+                                    "currency": {
+                                        "S": "USD"
+                                    },
+                                    "imageUrl": {
+                                        "S": "http://test.com/image_1.jpg"
+                                    },
+                                    "parseError": {
+                                        "NULL": True
+                                    }
+                                }
                             }
                         }
                     }
@@ -68,29 +76,34 @@ event = {
                 "NewImage": {
                     "document": {
                         "M": {
-                            "url": {
-                                "S": "http://test.com/product_2"
-                            },
-                            "store": {
-                                "S": "test_store"
-                            },
-                            "brand": {
-                                "S": "petzl"
-                            },
-                            "name": {
-                                "S": "sarken"
-                            },
-                            "price": {
-                                "N": 20.99
-                            },
-                            "currency": {
-                                "S": "USD"
-                            },
-                            "imageUrl": {
-                                "S": "http://test.com/image_2.jpg"
-                            },
-                            "parseError": {
-                                "S": "Some error"
+                            "kind": {"S": "product"},
+                            "data": {
+                                "M": {
+                                    "url": {
+                                        "S": "http://test.com/product_2"
+                                    },
+                                    "store": {
+                                        "S": "test_store"
+                                    },
+                                    "brand": {
+                                        "S": "petzl"
+                                    },
+                                    "name": {
+                                        "S": "sarken"
+                                    },
+                                    "price": {
+                                        "N": 20.99
+                                    },
+                                    "currency": {
+                                        "S": "USD"
+                                    },
+                                    "imageUrl": {
+                                        "S": "http://test.com/image_2.jpg"
+                                    },
+                                    "parseError": {
+                                        "S": "Some error"
+                                    }
+                                }
                             }
                         }
                     }
@@ -108,29 +121,34 @@ event = {
                 "NewImage": {
                     "document": {
                         "M": {
-                            "url": {
-                                "S": "http://test.com/product_3"
-                            },
-                            "store": {
-                                "S": "test_store"
-                            },
-                            "brand": {
-                                "S": "petzl"
-                            },
-                            "name": {
-                                "S": "petzl sarken"
-                            },
-                            "price": {
-                                "N": 30.99
-                            },
-                            "currency": {
-                                "S": "USD"
-                            },
-                            "imageUrl": {
-                                "S": "http://test.com/image_3.jpg"
-                            },
-                            "parseError": {
-                                "NULL": True
+                            "kind": {"S": "product"},
+                            "data": {
+                                "M": {
+                                    "url": {
+                                        "S": "http://test.com/product_3"
+                                    },
+                                    "store": {
+                                        "S": "test_store"
+                                    },
+                                    "brand": {
+                                        "S": "petzl"
+                                    },
+                                    "name": {
+                                        "S": "petzl sarken"
+                                    },
+                                    "price": {
+                                        "N": 30.99
+                                    },
+                                    "currency": {
+                                        "S": "USD"
+                                    },
+                                    "imageUrl": {
+                                        "S": "http://test.com/image_3.jpg"
+                                    },
+                                    "parseError": {
+                                        "NULL": True
+                                    }
+                                }
                             }
                         }
                     }
@@ -148,26 +166,31 @@ event = {
                 "NewImage": {
                     "document": {
                         "M": {
-                            "url": {
-                                "S": "http://test.com/product_4"
-                            },
-                            "store": {
-                                "S": "test_store"
-                            },
-                            "brand": {
-                                "S": "petzl"
-                            },
-                            "name": {
-                                "S": "petzl lynx"
-                            },
-                            "price": {
-                                "N": 40.99
-                            },
-                            "currency": {
-                                "S": "USD"
-                            },
-                            "parseError": {
-                                "NULL": True
+                            "kind": {"S": "product"},
+                            "data": {
+                                "M": {
+                                    "url": {
+                                        "S": "http://test.com/product_4"
+                                    },
+                                    "store": {
+                                        "S": "test_store"
+                                    },
+                                    "brand": {
+                                        "S": "petzl"
+                                    },
+                                    "name": {
+                                        "S": "petzl lynx"
+                                    },
+                                    "price": {
+                                        "N": 40.99
+                                    },
+                                    "currency": {
+                                        "S": "USD"
+                                    },
+                                    "parseError": {
+                                        "NULL": True
+                                    }
+                                }
                             }
                         }
                     }
@@ -190,6 +213,62 @@ event = {
     ]
 }
 
+original_product_event = {
+    "Records": [
+        {
+            "eventName": "INSERT",
+            "dynamodb": {
+                "Keys": {
+                    "url": {
+                        "S": "http://test.com/original_category_1"
+                    }
+                },
+                "NewImage": {
+                    "document": {
+                        "M": {
+                            "data": {
+                                "M": {
+                                    "nextURL": {
+                                        "S": "https://grivel.com/collections/crampons?page=3"
+                                    },
+                                    "products": {
+                                        "L": [
+                                            {
+                                                "M": {
+                                                    "brand": {
+                                                        "S": "grivel"
+                                                    },
+                                                    "name": {
+                                                        "S": "Spider"
+                                                    }
+                                                }
+                                            },
+                                            {
+                                                "M": {
+                                                    "brand": {
+                                                        "S": "grivel"
+                                                    },
+                                                    "name": {
+                                                        "S": "Ran"
+                                                    }
+                                                }
+                                            }
+                                        ]
+                                    }
+                                }
+                            },
+                            "kind": {
+                                "S": "originalCategory"
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    ]
+}
+
+
 
 def get_es_instance():
     elasticsearch = Elasticsearch(ELASTIC_HOST, verify_certs=False)
@@ -205,20 +284,38 @@ def get_es_instance():
         raise RuntimeError("Elasticsearch failed to start.")
 
 
-es = get_es_instance()
-
-
 class TestDataTransfer(unittest.TestCase):
+
+    es = get_es_instance()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.es.indices.delete(index=PRODUCT_INDEX, ignore=[400, 404])
+        cls.es.indices.delete(index=ORIGINAL_INDEX, ignore=[400, 404])
 
     # TODO enable the local model storage
     def test_indexing(self):
         lambda_handler(event, None)
-        product_1_expected = {'url': 'http://test.com/product_1', 'store': 'test_store', 'name': 'lynx', 'normalizedName': 'lynx', 'price': 10.99, 'oldPrice': 12.99, 'currency': 'USD', 'imageUrl': 'http://test.com/image_1.jpg'}
-        product_3_expected = {'url': 'http://test.com/product_3', 'store': 'test_store', 'name': 'petzl sarken', 'normalizedName': 'sarken', 'price': 30.99, 'currency': 'USD', 'imageUrl': 'http://test.com/image_3.jpg'}
+        product_1_expected = {'url': 'http://test.com/product_1', 'store': 'test_store', 'name': 'lynx', 'original': 'lynx', 'brand': 'petzl', 'price': 10.99, 'oldPrice': 12.99, 'currency': 'USD', 'imageUrl': 'http://test.com/image_1.jpg'}
+        product_3_expected = {'url': 'http://test.com/product_3', 'store': 'test_store', 'name': 'petzl sarken', 'original': 'sarken', 'brand': 'petzl', 'price': 30.99, 'currency': 'USD', 'imageUrl': 'http://test.com/image_3.jpg'}
         assert(self.get_actual("http://test.com/product_1") == product_1_expected)
         assert(self.get_actual("http://test.com/product_3") == product_3_expected)
 
-    @staticmethod
-    def get_actual(doc_id):
-        return es.get(index=INDEX, doc_type=DOC_TYPE, id=doc_id)["_source"]
+    @classmethod
+    def get_actual(cls, doc_id):
+        return cls.es.get(index=PRODUCT_INDEX, doc_type=PRODUCT_DOC_TYPE, id=doc_id)["_source"]
+
+    def test_original_indexing(self):
+        lambda_handler(original_product_event, None)
+        # original_1_expected = {'url': 'http://test.com/original_category_1', 'store': 'test_store', 'name': 'lynx', 'original': 'lynx', 'brand': 'petzl', 'price': 10.99, 'oldPrice': 12.99, 'currency': 'USD', 'imageUrl': 'http://test.com/image_1.jpg'}
+        sleep(1)
+        response = self.es.search(
+            index=ORIGINAL_INDEX,
+            body={
+                "query": {"match_all": {}}
+            }
+        )
+        print(response)
+        # assert response["count"] == 2
+
 
