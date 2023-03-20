fc6c30e09ba56acfdc2cb105810155fbab937549
Ivan Zolotov <vanchpuck@mail.ru>
2023-03-21 00:45:40 +0300

    Add ES request signing

--- lambda_function.py
+++ lambda_function.py
@@ -2,6 +2,8 @@ import logging
 import os
 
 from util import *
+from requests_aws4auth import AWS4Auth
+from elasticsearch import Elasticsearch, RequestsHttpConnection
 
 logging.getLogger().setLevel(logging.INFO)
 boto3.resource('dynamodb')
@@ -12,7 +14,17 @@ CLASSIFIER = Classifier(MODEL_DATA_BUCKET)
 PRODUCT_INDEX = "products"
 
 ES_HOST = os.environ.get('ES_HOST', '')
-ES = Elasticsearch(ES_HOST, verify_certs=False)
+ES_SERVICE = 'es'
+CREDENTIALS = boto3.Session().get_credentials()
+AWS_ES_AUTH = AWS4Auth(CREDENTIALS.access_key, CREDENTIALS.secret_key, os.environ.get(
+    "AWS_REGION", ""), ES_SERVICE, session_token=CREDENTIALS.token)
+ES = Elasticsearch(
+    ES_HOST,
+    http_auth=AWS_ES_AUTH,
+    use_ssl=True,
+    verify_certs=False,
+    connection_class=RequestsHttpConnection
+)
 
 
 def lambda_handler(event, context):
